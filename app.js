2569c6200b014b722ea7d8ef8d572fb3f1dbc0f1
Deo Mareza <deomareza@gmail.com>
2020-11-14 15:49:58 +0700

    changed port in app.js to process.env.PORT

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ if(process.env.NODE_ENV !== "production"){
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const router = require('./routers/')
 const cors = require('cors')
 const errorHandler = require('./middlewares/errorHandler')
